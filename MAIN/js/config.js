05d3e79de5ff9f265b8ef2136d05e50166dc209e
JawBusHunter <jawbushunter2@outlook.fr>
2026-07-19 22:42:25 +0200

    Delete MAIN/js/config.js

--- MAIN/js/config.js
+++
@@ -1,5 +0,0 @@
-window.BAZAAR_LEDGER_CONFIG = {
-  supabaseUrl: 'https://jivjfjkcgibmurqdqnhe.supabase.co',
-  supabaseAnonKey: 'sb_publishable_uRIWeBHumSFOezcyD3BXww_4h0jL9KD',
-  roomId: 'mon-groupe-flips'
-};
